78e4fd446fadf3f2bfc6b1d2e7bef3817e31f12d
Taoning Ge <gtn1024@foxmail.com>
2024-05-25 20:54:23 +0800

    add shadcn-ui and react router dom

---
+++ web/src/routes/router.tsx
@@ -0,0 +1,14 @@
+import {createBrowserRouter} from "react-router-dom";
+
+export const router = createBrowserRouter(
+  [
+    {
+      path: '/',
+      element: (<div>This is home page</div>)
+    },
+    {
+      path: '*',
+      element: (<>404</>)
+    },
+  ]
+)
